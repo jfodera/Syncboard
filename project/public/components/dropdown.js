7b78ed0737258aaf225b464fcb2a5fbbe1f32d83
Grace <gracehui30@gmail.com>
2025-02-25 08:29:20 -0500

    Color Change
    Color of button changes based on selection

--- project/public/components/dropdown.js
+++ project/public/components/dropdown.js
@@ -1,26 +1,27 @@
-
-
 const DropdownSelect = ({ id, name }) => {
-    const [color, setColor] = React.useState();
+    const [color, setColor] = React.useState("");
 
-    // onClick: () => { 
-    //     setColor();
-    // }
-
-    // const handleChange = (e) => {
-    //     const { name, value } = e.target;
-    //     setFormData({
-    //       ...formData,
-    //       [name]: value
-    //     });
-    //   };
+    const handleChange = (e) => {
+        const selectedValue = e.target.value;
+     
+        switch (selectedValue) {
+            case "1":
+                setColor("var(--blue)"); 
+                break;
+            case "2":
+                setColor("var(--yellow)"); 
+                break;
+            default:
+                setColor("");
+        }
+    };
 
     return (
         <div className="select">
-            <select name={name} id={id}>
+            <select name={name} id={id} onChange={handleChange} style={{ backgroundColor: color }}>
                 <option>Status</option>
-                <option className="blue" value="1" onChange={handleChange}>Completed</option>
-                <option value="2" onClick='setColor("yellow")'>In Progress</option>
+                <option value="1">Completed</option>
+                <option value="2">In Progress</option>
                 <option selected value="3">To Do</option>
             </select>
         </div>
