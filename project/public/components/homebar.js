e57937d9b4178eb0e0b31248f3cfd7aaa97eb4bc
Morgan Myers <myersm6@rpi.edu>
2025-04-15 13:46:49 -0400

    Update homebar.js

--- project/public/components/homebar.js
+++ project/public/components/homebar.js
@@ -14,10 +14,11 @@ const Homebar = () => {
 
             <div className={`bar-links ${isOpen ? 'open' : ''}`}>
                 <ReactRouterDOM.Link to="/class">Team Dashboard</ReactRouterDOM.Link>
-                <a href="#">Chat</a>
+                <ReactRouterDOM.Link to="/class/chat">Chat</ReactRouterDOM.Link>
                 <ReactRouterDOM.Link to="/class/resources">Resources</ReactRouterDOM.Link>
                 <ReactRouterDOM.Link to="/class/profile">Profile</ReactRouterDOM.Link>
             </div>
         </nav>
     );
 };
+
